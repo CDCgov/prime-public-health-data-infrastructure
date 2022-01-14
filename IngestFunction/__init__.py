108890bd3fa28d11cfa6a364425053b8869062c2
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2022-01-14 12:19:27 -0500

    first attempt at ingest function

---
+++ IngestFunction/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import pysftp
+import sys
+import azure.functions as func
+from .settings import settings
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+    logging.info(f"Settings: {settings}")
+
+    try:
+        sftp = pysftp.Connection(
+            settings["hostname"],
+            username=settings["username"],
+            password=settings["password"],
+        )
+        for filename in sftp.listdir("/"):
+            logging.info(f"filename: {filename}")
+
+        return func.HttpResponse(f"This HTTP triggered function executed successfully.")
+    except:
+        e = sys.exc_info()
+        logging.error(f"Exception: {e}, Traceback: {e[2]}")
+        return func.HttpResponse(f"Error in response: {e}", status_code=500)
---
+++ IngestFunction/settings.py
@@ -0,0 +1,7 @@
+import os
+
+settings = {
+    "hostname": os.environ["VDHSFTPHostname"],
+    "username": os.environ["VDHSFTPUsername"],
+    "password": os.environ["VDHSFTPPassword"],
+}
