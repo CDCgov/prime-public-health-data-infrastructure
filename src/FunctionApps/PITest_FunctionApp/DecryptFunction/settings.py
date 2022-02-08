3444bdb3f06fe8e231fee04b8ee02d29c495cc4c
Brett Maden <brett.m.maden@omb.eop.gov>
2022-02-08 09:56:55 -0500

    make comment about the change to DecryptSettings behavior more clear

--- src/FunctionApps/PITest_FunctionApp/DecryptFunction/settings.py
+++ src/FunctionApps/PITest_FunctionApp/DecryptFunction/settings.py
@@ -8,7 +8,9 @@ class DecryptSettings:
     When run remotely, settings are passed by environment variables in the function app, 
     which in turn reference KeyVault variables as mentioned here: https://docs.microsoft.com/en-us/azure/app-service/app-service-key-vault-references
 
-    When tests are run, they consult the local file `test.settings.json`.
+    When tests are run, these settings are read from the local file `test.settings.json`
+    which contains a different pgp key that is only used for tests and is not stored in
+    the KeyVault.
     """
     private_key_password = os.getenv("PrivateKeyPassword")
     private_key = os.getenv("PrivateKey")
