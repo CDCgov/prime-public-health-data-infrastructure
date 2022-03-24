d5c821642338f4e981987fa3637f828b9b7769d5
Josiah Siegel <josiah.siegel@semanticbits.com>
2022-03-24 14:22:18 -0400

    lock tf azure provider to 2.99 to avoid 3.0 breaking changes

--- operations/app/terraform/modules/function_app/functions.tf
+++ operations/app/terraform/modules/function_app/functions.tf
@@ -1,29 +1,36 @@
 # Devops functions
 
-data "archive_file" "devops_function_app" {
-  type        = "zip"
-  source_dir  = "../../../../../src/FunctionApps/DevOps"
-  output_path = "devops-function-app.zip"
-}
-
 locals {
-  publish_devops_command = <<EOF
+  devops_function_path   = "../../../../../src/FunctionApps/DevOps"
+  devops_publish_command = <<EOF
       az functionapp deployment source config-zip --resource-group ${var.resource_group_name} \
       --name ${azurerm_function_app.pdi_infrastructure.name} --src ${data.archive_file.devops_function_app.output_path} \
       --build-remote false
     EOF
 }
 
+data "archive_file" "devops_function_app" {
+  type        = "zip"
+  source_dir  = local.devops_function_path
+  output_path = "devops-function-app.zip"
+
+  excludes = [
+    ".venv",
+    ".vscode",
+    "local.settings.json"
+  ]
+}
+
 resource "null_resource" "devops_function_app_publish" {
   provisioner "local-exec" {
-    command = local.publish_devops_command
+    command = local.devops_publish_command
   }
   depends_on = [
-    local.publish_devops_command,
+    local.devops_publish_command,
     azurerm_function_app.pdi_infrastructure
   ]
   triggers = {
     input_json           = filemd5(data.archive_file.devops_function_app.output_path)
-    publish_code_command = local.publish_devops_command
+    publish_code_command = local.devops_publish_command
   }
 }
--- operations/app/terraform/vars/test/azure.tf
+++ operations/app/terraform/vars/test/azure.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.99.0" # This version must also be changed in other environments
+      version = "= 2.99.0" # 3.0 contains breaking changes!
     }
   }
 
