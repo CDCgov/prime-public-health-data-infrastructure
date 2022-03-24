33b9d5ec14a274fce6e7e989ad2274218a7e2a6c
Josiah Siegel <josiah.siegel@semanticbits.com>
2022-03-24 12:22:37 -0400

    storage access function uses azure identity

--- src/FunctionApps/DevOps/ConfirmStorageAccess/__init__.py
+++ src/FunctionApps/DevOps/ConfirmStorageAccess/__init__.py
@@ -1,12 +1,17 @@
 from azure.storage.blob import BlobServiceClient
-from azure.identity import ManagedIdentityCredential
+from azure.identity import DefaultAzureCredential
 import azure.functions as func
+import os
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
-    creds = ManagedIdentityCredential ()
+    creds = DefaultAzureCredential()
 
-    blob_service_client = BlobServiceClient(account_url="https://pidevdatasa.blob.core.windows.net/", credential=creds)
-    containers = blob_service_client.list_containers()
+    storage_account=os.getenv('AzureWebJobsStorage__accountName')
+    print (f"Storage account name: {storage_account}")
+    blob_service_client = BlobServiceClient(account_url=f"https://{storage_account}.blob.core.windows.net/", credential=creds)
 
-    return func.HttpResponse(f"Containers: {containers}")
+    containers = list(blob_service_client.list_containers())
+
+    return func.HttpResponse(f"{storage_account} : {containers}")
