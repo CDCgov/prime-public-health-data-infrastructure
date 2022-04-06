d4eb233728d6c7d6593cdbb58ac479ae789cbaad
Spencer Kathol <spencer@kathol.net>
2022-04-06 06:56:13 -0600

    Remove multithreading and add support for additinoal input formats

--- src/FunctionApps/BulkImport/Import/main.py
+++ src/FunctionApps/BulkImport/Import/main.py
@@ -1,82 +1,137 @@
 import logging
 import os
 import zipfile
-import azure.functions as func
+import io
 
 import requests
-import multiprocessing
 import json
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3 import Retry
 
 unzipped_directory = "./FhirResources"
 
 
-def main(zip_file):
-    unzip_input_file(zip_file)
-    process_ndjson_files(unzipped_directory)
+def main(file):
+    # Default fhir_location to be the input file
+    fhir_location = file
+
+    # If input file is a zip file, unzip it, and process the resulting directory
+    if zipfile.is_zipfile(file):
+        fhir_location = _unzip_input_file(file)
+    
+    # Process input FHIR file or directory
+    process_fhir(fhir_location)
 
 
-def unzip_input_file(zip_file):
-    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+def _unzip_input_file(zip_filepath: str) -> str:
+    with zipfile.ZipFile(zip_filepath, "r") as zip_ref:
         zip_ref.extractall(unzipped_directory)
+        return unzipped_directory
 
-
-def process_ndjson_files(dir):
-    with multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1) as p:
-        directory = os.fsencode(dir)
-        for file in os.listdir(directory):
+def process_fhir(path: str):
+    path = os.fsencode(path)
+    if os.isdir(path):
+        for file in os.listdir(path):
             filename = os.fsdecode(file)
-            if filename.endswith(".ndjson"):
-                file_path = os.path.join(dir, filename)
-                p.apply_async(read_file, (file_path,))
-            else:
-                logging.error(f"File must be ndjson, file given: {filename}")
-        p.close()
-        p.join()
-
-
-def read_file(file):
-    with open(file) as fp:
+            #; Recursively call on files within directory
+            process_fhir(filename)
+    elif(path.endswith(".ndjson")):
+        process_fhir_ndjson(path)
+    else:
+        process_fhir_resource(path)
+        
+def process_fhir_ndjson(filename: str):
+    with open(filename) as fp:
         for line in fp:
-            json_line = json.loads(line)
-            resource_type = json_line["resourceType"]
-            resource_id = json_line["id"]
-            post_to_fhir(line, resource_type, resource_id)
+            import_to_fhir(line)
 
+def process_fhir_resource(fhir_filepath: str = "", fhir_string: str = ""):
+    if os.path.exists(fhir_filepath):
+        fhir_string = open(fhir_filepath).read()
 
-def post_to_fhir(line, resource_type, resource_id):
+    import_to_fhir(fhir_string)
+    
+
+def import_to_fhir(json_string: str, method: str = "PUT"):
     try:
         token = get_access_token()
     except Exception:
-        return func.HttpResponse("error getting access token", status_code=401)
+        logging.exception("Failed to get access token")
+        raise requests.exceptions.HTTPError("Authorization error occurred while processing information into FHIR server.")
     retry_strategy = Retry(
         total=3,
         status_forcelist=[429, 500, 502, 503, 504],
-        allowed_methods=["HEAD", "POST", "OPTIONS"],
+        allowed_methods=["HEAD", "PUT", "POST", "OPTIONS"],
     )
     adapter = HTTPAdapter(max_retries=retry_strategy)
     http = requests.Session()
     http.mount("https://", adapter)
     http.mount("http://", adapter)
-    url = os.environ.get("FHIR_URL", "")
+    fhir_url = os.environ.get("FHIR_URL", "")
     try:
-        resp = requests.post(
-            f"{url}/{resource_type}",
-            headers={
-                "Authorization": f"Bearer {token}",
-                "Accept": "application/fhir+json",
-                "Content-type": "application/json",
-            },
-            data=line,
-        )
-        print(f"status={resp.status_code} message={resource_id}")
-    except Exception:
-        return func.HttpResponse("ndjson to fhir import failed", status_code=500)
-        logging.error(
-            f"Failed to import ndjson to FHIR server failed status={resp.status_code} message={resp.text}"  # noqa
-        )
+        json_object = json.loads(json_string)
+        resource_id = json_object["resource_id"]
+        resource_type = json_object["resource_type"]
 
+        if resource_type == "Bundle":
+            transaction_json = _ensure_bundle_transaction(json_object,method,url)
+            resp = requests.post(
+                fhir_url,
+                headers={
+                    "Authorization": f"Bearer {token}",
+                    "Accept": "application/fhir+json",
+                    "Content-Type": "application/fhir+json",
+                },
+                data=json.dumps(transaction_json),
+            )
+            print(f"status={resp.status_code} message={resource_id}")
+        elif method == "POST":
+            resp = requests.post(
+                f"{fhir_url}/{resource_type}",
+                headers={
+                    "Authorization": f"Bearer {token}",
+                    "Accept": "application/fhir+json",
+                    "Content-Type": "application/fhir+json",
+                },
+                data=json_string,
+            )
+            print(f"status={resp.status_code} message={resource_id}")
+        elif method == "PUT":
+            resp = requests.put(
+                f"{fhir_url}/{resource_type}/{resource_id}",
+                headers={
+                    "Authorization": f"Bearer {token}",
+                    "Accept": "application/fhir+json",
+                    "Content-Type": "application/fhir+json",
+                },
+                data=json_string
+            )
+
+    except Exception:
+        logging.exception("Request using method " + method + " failed for json: " + json_string)
+        return 
+
+def _ensure_bundle_transaction(json_object : dict, method: str, fhir_url: str) -> dict:
+    if json_object["resourceType"] != "Bundle":
+        raise ValueError("_ensure_bundle_transaction called on non-Bundle resource: " + json_object["resourceType"])
+
+    if method not in ("PUT","POST"):
+        raise ValueError("_ensure_bundle_transaction only supports PUT and POST methods")
+
+    new_bundle = json_object.deepcopy()
+    new_bundle["type"] = "transaction"
+
+    for entry in new_bundle["entry"]:
+        url = fhir_url + "/" + entry["resource"]["resourceType"]
+
+        # PUT requires the URL to contain the resource ID, POST does not.
+        if method == "PUT":
+            url += "/" + entry["resource"]["id"]
+
+        entry["request"] = { "method": method,
+                            "url": url}
+        
+    return new_bundle
 
 def get_access_token() -> str:
     """Get the access token based on creds in the environment"""
@@ -99,7 +154,3 @@ def get_access_token() -> str:
         f"access token request failed status={resp.status_code} message={resp.text}"
     )
     raise Exception("access token request failed")
-
-
-if __name__ == "__main__":
-    main("../../PITest_FunctionApp/tests/assets/Archive.zip")
--- src/FunctionApps/BulkImport/tests/test_import_function.py
+++ src/FunctionApps/BulkImport/tests/test_import_function.py
@@ -1,40 +1,90 @@
-import os
+import unittest
+
 from unittest import mock
+from Import import main
+import requests
+
+import os
+import shutil
 from pathlib import Path
 
-from Import.main import unzip_input_file
-from Import.main import get_access_token
+class TestImport(unittest.TestCase):
+    TEST_ENV = {
+        "TENANT_ID": "a-tenant-id",
+        "CLIENT_ID": "a-client-id",
+        "CLIENT_SECRET": "a-client-secret",
+        "FHIR_URL": "https://some-fhir-server",
+    }
 
-TEST_ENV = {
-    "TENANT_ID": "a-tenant-id",
-    "CLIENT_ID": "a-client-id",
-    "CLIENT_SECRET": "a-client-secret",
-    "FHIR_URL": "https://some-fhir-server",
-}
+    @mock.patch("requests.post")
+    @mock.patch.dict("os.environ", TEST_ENV)
+    def test_get_access_token(self, mock_post):
+        resp = mock.Mock()
+        resp.json.return_value = {"access_token": "some-access-token"}
+        mock_post.return_value = resp
+
+        assert "some-access-token" == main.get_access_token()
+
+        mock_post.assert_called_with(
+            "https://login.microsoftonline.com/a-tenant-id/oauth2/token",
+            data={
+                "grant_type": "client_credentials",
+                "client_id": "a-client-id",
+                "client_secret": "a-client-secret",
+                "resource": "https://some-fhir-server",
+            },
+        )
+
+    @mock.patch("requests.post")
+    @mock.patch.dict("os.environ", TEST_ENV)
+    def test_fhir_post(self, mock_post):
+        token_resp = mock.Mock()
+        token_resp.json.return_value = {"access_token": "some-access-token"}
+
+        fhir_resp = mock.Mock()
+        fhir_resp.json.return_value = {"resourceType": "Patient", "id": "test-id"}
+        
+        mock_post.side_effect = [token_resp, fhir_resp]
+
+        url = os.environ.get("FHIR_URL")
+        calls = [mock.call(
+            "https://login.microsoftonline.com/a-tenant-id/oauth2/token",
+            data={
+                "grant_type": "client_credentials",
+                "client_id": "a-client-id",
+                "client_secret": "a-client-secret",
+                "resource": "https://some-fhir-server",
+            }),
+            mock.call(
+                f"{url}/Patient",
+                headers={
+                    "Authorization": mock.ANY,
+                    "Accept": "application/fhir+json",
+                    "Content-Type": "application/fhir+json",
+                },
+                data={"resourceType": "Patient", 
+                "id": "test-id"},                
+            )]
+
+        main.process_fhir_resource(fhir_string = "{\"resourceType\": \"Patient\", \"id\": \"test-id\"}")
+
+        mock_post.assert_called_with(
+            f"{url}/Patient",
+            headers={
+                "Authorization": mock.ANY,
+                "Accept": "application/fhir+json",
+                "Content-Type": "application/fhir+json",
+            },
+            data={"resourceType": "Patient", "id": "test-id"},
+        )
 
 
-@mock.patch("requests.post")
-@mock.patch.dict("os.environ", TEST_ENV)
-def test_get_access_token(mock_post):
-    resp = mock.Mock()
-    resp.json.return_value = {"access_token": "some-access-token"}
-    mock_post.return_value = resp
+    def test_file_unzip(self):
+        test_file_path = Path("BulkImport").parent / "tests" / "assets" / "test_files.zip"
+        main._unzip_input_file(test_file_path)
+        assert os.path.isfile("./FhirResources/test_files/Claim-1.ndjson")
+        assert os.path.isfile("./FhirResources/test_files/Organization-1.ndjson")
+        shutil.rmtree("./FhirResources")
 
-    assert "some-access-token" == get_access_token()
-
-    mock_post.assert_called_with(
-        "https://login.microsoftonline.com/a-tenant-id/oauth2/token",
-        data={
-            "grant_type": "client_credentials",
-            "client_id": "a-client-id",
-            "client_secret": "a-client-secret",
-            "resource": "https://some-fhir-server",
-        },
-    )
-
-
-def test_file_unzip():
-    test_file_path = Path("BulkImport").parent / "tests" / "assets" / "test_files.zip"
-    unzip_input_file(test_file_path)
-    assert os.path.isfile("./FhirResources/test_files/Claim-1.ndjson")
-    assert os.path.isfile("./FhirResources/test_files/Organization-1.ndjson")
+if __name__ == '__main__':
+    unittest.main()
