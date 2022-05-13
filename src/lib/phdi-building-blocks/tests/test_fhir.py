d28abe6a1a19ebd3356d6d4c8d6d594eaaa985da
Spencer Kathol <spencer@skylight.digital>
2022-05-13 04:57:06 -0600

    Add unit tests

--- src/lib/phdi-building-blocks/tests/test_fhir.py
+++ src/lib/phdi-building-blocks/tests/test_fhir.py
@@ -1,3 +1,4 @@
+import io
 import os
 import pytest
 import polling
@@ -9,6 +10,7 @@ from unittest import mock
 
 from phdi_building_blocks.fhir import (
     _compose_export_url,
+    download_from_export_response,
     export_from_fhir_server,
     store_data,
     get_fhirserver_cred_manager,
@@ -289,3 +291,56 @@ def test_compose_export_url():
 
     with pytest.raises(ValueError):
         _compose_export_url(fhir_url, "InvalidExportScope")
+
+
+@mock.patch("phdi_building_blocks.fhir._download_export_blob")
+def test_download_from_export_response(mock_download_export_blob):
+    mock_download_export_blob.side_effect = [
+        io.TextIOWrapper(
+            io.BytesIO(
+                b'{"resourceType": "Patient", "id": "some-id"}\n'
+                + b'{"resourceType": "Patient", "id": "some-id2"}\n'
+            ),
+            encoding="utf-8",
+            newline="\n",
+        ),
+        io.TextIOWrapper(
+            io.BytesIO(
+                b'{"resourceType": "Observation", "id": "some-id"}\n'
+                + b'{"resourceType": "Observation", "id": "some-id2"}\n'
+            ),
+            encoding="utf-8",
+            newline="\n",
+        ),
+    ]
+
+    export_response = {
+        "output": [
+            {"type": "Patient", "url": "https://export-download-url/_Patient"},
+            {
+                "type": "Observation",
+                "url": "https://export-download-url/_Observation",
+            },
+        ]
+    }
+
+    for type, output in download_from_export_response(export_response=export_response):
+        if type == "Patient":
+            assert (
+                output.read()
+                == '{"resourceType": "Patient", "id": "some-id"}\n'
+                + '{"resourceType": "Patient", "id": "some-id2"}\n'
+            )
+        elif type == "Observation":
+            assert (
+                output.read()
+                == '{"resourceType": "Observation", "id": "some-id"}\n'
+                + '{"resourceType": "Observation", "id": "some-id2"}\n'
+            )
+
+    mock_download_export_blob.assert_has_calls(
+        [
+            mock.call(blob_url="https://export-download-url/_Patient"),
+            mock.call(blob_url="https://export-download-url/_Observation"),
+        ]
+    )
