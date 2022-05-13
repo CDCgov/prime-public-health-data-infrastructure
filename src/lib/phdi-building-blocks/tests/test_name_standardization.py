6184693871922f86bae0fa3cd6da7dc1e69ee470
DanielPaseltiner <daniel.paseltiner@gmail.com>
2022-05-13 11:38:50 -0400

    Add unit test for geocode_patient_address().

--- src/lib/phdi-building-blocks/tests/test_name_standardization.py
+++ src/lib/phdi-building-blocks/tests/test_name_standardization.py
@@ -1,9 +1,11 @@
+import json
+import pathlib
+import copy
+
 from phdi_building_blocks.name_standardization import (
     standardize_name,
     standardize_patient_name,
 )
-import json
-import pathlib
 
 
 def test_standardize_name():
@@ -15,7 +17,7 @@ def test_standardize_patient_name():
     raw_bundle = json.load(
         open(pathlib.Path(__file__).parent / "assets" / "patient_bundle.json")
     )
-    standardized_bundle = raw_bundle.copy()
+    standardized_bundle = copy.deepcopy(raw_bundle)
     patient = standardized_bundle["entry"][1]["resource"]
     patient["name"][0]["family"] = "DOE"
     patient["name"][0]["given"] = ["JOHN", "DANGER"]
