3a0ead61adc6becc3a1b3143ea0facdd61b5c527
DanielPaseltiner <daniel.paseltiner@gmail.com>
2022-05-24 10:40:23 -0400

    Address comments from code review excluding doc string re-format and fuzzy matching.

--- src/lib/phdi-building-blocks/phdi_building_blocks/standardize.py
+++ src/lib/phdi-building-blocks/phdi_building_blocks/standardize.py
@@ -1,6 +1,6 @@
 import phonenumbers
 import pycountry
-from typing import Callable, Literal, Generator
+from typing import Callable, Literal, Generator, List
 
 from phdi_building_blocks.utils import find_patient_resources
 
@@ -64,7 +64,7 @@ def standardize_patient_name(
 
 def country_extractor(
     resource: dict, code_type: Literal["alpha_2", "alpha_3", "numeric"] = "alpha_2"
-) -> Generator[str, None, None]:
+) -> List[str]:
     """
     Given a FHIR resource yield a generator containing all of the counries found in the
     resource in a standard form sepcified by code_type.
@@ -74,22 +74,22 @@ def country_extractor(
         code_type (str): A string equal to 'alpha_2', 'alpha_3', or 'numeric' to specify
                          which type of standard country identifier to generate.
     Returns:
-        (generator): A generator to return standardized countries when it is iterated
-                     over.
+        countries (list): A list containing country codes as specified by code_type for
+        each country found in resource.
 
     We currently only handle extracting countries from patient resources, however as
     need arises is the future funcionality can be expanded.
     """
+    countries = []
     resource_type = resource.get("resource").get("resourceType")
     if resource_type == "Patient":
         for address in resource.get("resource").get("address"):
             country = address.get("country")
-            yield standardize_country(country, code_type)
+            countries.append(standardize_country(country, code_type))
+    return countries
 
 
-def standardize_phone(
-    raw: str, resource: dict = None, country_extractor: Callable = country_extractor
-) -> str:
+def standardize_phone(raw: str, countries: List = [None, "US"]) -> str:
     """
     Given a phone number and optionally an associated FHIR resource and country
     extraction function if able to parse the phone number return it in the E.164
@@ -98,13 +98,13 @@ def standardize_phone(
     Phone number parsing process:
     A maximum of three attemtps are made to parse any phone number.
     First, we try to parse the phone number without any additional country information.
-    If this succeeds it must have provided in a standard inernational format which is
-    the ideal case. If the first attempt fails and a FHIR resource associated with the
-    phone number and a country extraction function are provided we make a second attempt
-    to parse the phone number using any countries extracted from the resource. Finally,
-    in the case where the second attempt fails for a resource and country extraction
-    function are not provided we make a final attempt to parse the number assuming it
-    is American.
+    If this succeeds it must have been provided in a standard inernational format which
+    is the ideal case. If the first attempt fails and a FHIR resource associated with
+    the phone number and a country extraction function are provided we make a second
+    attempt to parse the phone number using any countries extracted from the resource.
+    Finally, in the case where the second attempt fails for a resource and country
+    extraction function are not provided we make a final attempt to parse the number
+    assuming it is American.
 
     Args:
         raw (str): Raw phone number to be standardized.
@@ -113,88 +113,68 @@ def standardize_phone(
                          contains information indicating which country the phone number
                          is from.
 
-        country_extractor (Callable): A function to parse a provided resource that
-                                      yields a generator to return the standard 2 letter
-                                      abreviation of each country found in the resource
-                                      when itterated over.
+        countries (list): A list containing 2 letter ISO country codes for each country
+                          extracted from resource of the phone number to be standardized
+                          that might indicate it the phone numbers country of origin.
     Returns:
         standardized (str): The standardized phone number in E.164 format when the raw
                             phone number was succesfully parsed and an emptry string
                             otherwise.
     """
 
-    phone_number = None
+    if countries != [None, "US"]:
+        countries = [None] + countries + ["US"]
 
-    # First Attempt - Assume complete international format provided.
-    try:
-        phone_number = phonenumbers.parse(raw, None)
-    except phonenumbers.phonenumberutil.NumberParseException:
-
-        # Second Attempt - If a resource and country extraction function have been
-        #                  provided attempt to parse using countries found in resource.
-        if resource is not None and country_extractor is not None:
-            for country in country_extractor(resource):
-                try:
-                    phone_number = phonenumbers.parse(raw, country)
-                    break
-                except phonenumbers.phonenumberutil.NumberParseException:
-                    continue
-
-        # Third Attempt - Assume the phone number is American.
-        if phone_number is None:
-            try:
-                phone_number = phonenumbers.parse(raw, "US")
-            except phonenumbers.phonenumberutil.NumberParseException:
-                standardized = ""
-
-    if phone_number is not None:
+    standardized = ""
+    for country in countries:
+        try:
+            standardized = phonenumbers.parse(raw, country)
+        except phonenumbers.phonenumberutil.NumberParseException:
+            continue
+    if standardized != "":
         standardized = str(
             phonenumbers.format_number(
-                phone_number, phonenumbers.PhoneNumberFormat.E164
+                standardized, phonenumbers.PhoneNumberFormat.E164
             )
         )
-
     return standardized
 
 
 def standardize_country(
-    country: str, code_type: Literal["alpha_2", "alpha_3", "numeric"] = "alpha_2"
+    raw_country: str, code_type: Literal["alpha_2", "alpha_3", "numeric"] = "alpha_2"
 ) -> str:
     """
     Given a country return it in a standard form as specified by code_type.
 
     Args:
-        country (str): Country to be standardized.
+        raw_country (str): Country to be standardized.
         code_type (str): A string equal to 'alpha_2', 'alpha_3', or 'numeric' to specify
                          which type of standard country identifier to generate.
 
     Returns:
-        (str): Country in standardized form, or None if unable to standardize.
+        standard_country (str): Country in standardized form, or None if unable to standardize.
     """
-    if len(country) == 3 and country.isalpha():
-        country = pycountry.countries.get(alpha_3=country)
-    elif len(country) == 2 and country.isalpha():
-        country = pycountry.countries.get(alpha_2=country)
-    elif len(country) == 3 and country.isnumeric():
-        country = pycountry.countries.get(numeric=country)
-    elif len(country) > 4 and country.isalpha:
-        possible_countries = pycountry.countries.search_fuzzy(country)
+    standard_country = ""
+    if len(raw_country) == 3 and raw_country.isalpha():
+        standard_country = pycountry.countries.get(alpha_3=raw_country)
+    elif len(raw_country) == 2 and raw_country.isalpha():
+        standard_country = pycountry.countries.get(alpha_2=raw_country)
+    elif len(raw_country) == 3 and raw_country.isnumeric():
+        standard_country = pycountry.countries.get(numeric=raw_country)
+    elif len(raw_country) >= 4 and all(x.isalpha() or x.isspace() for x in raw_country):
+        possible_countries = pycountry.countries.search_fuzzy(raw_country)
         if len(possible_countries) == 1:
-            country = possible_countries[0]
-        else:
-            country = ""
-    else:
-        country = ""
+            standard_country = possible_countries[0]
 
-    if country != "":
+    if standard_country != "":
         if code_type == "alpha_2":
-            country = country.alpha_2
+            standard_country = standard_country.alpha_2
         elif code_type == "alpha_3":
-            country = country.alpha_3
+            standard_country = standard_country.alpha_3
         elif code_type == "numeric":
-            country = country.numeric
+            standard_country = standard_country.numeric
 
-    return country
+    return standard_country
 
 
 def standardize_patient_phone(
@@ -212,9 +192,10 @@ def standardize_patient_phone(
         # Transform phone numbers
         raw_phones = []
         std_phones = []
+        countries = country_extractor(resource)
         for telecom in patient.get("telecom", []):
             if telecom.get("system") == "phone" and "value" in telecom:
-                transformed_phone = standardize(telecom["value"], resource)
+                transformed_phone = standardize(telecom["value"], countries)
                 raw_phones.append(telecom["value"])
                 std_phones.append(transformed_phone)
                 telecom["value"] = transformed_phone
--- src/lib/phdi-building-blocks/tests/test_standardize.py
+++ src/lib/phdi-building-blocks/tests/test_standardize.py
@@ -47,9 +47,10 @@ def test_standardize_phone():
         open(pathlib.Path(__file__).parent / "assets" / "patient_bundle.json")
     )
     patient = raw_bundle["entry"][1]
+    countries = country_extractor(patient)
 
     assert standardize_phone("+11234567890") == "+11234567890"
-    assert standardize_phone("(123)-456-7890", patient) == "+11234567890"
+    assert standardize_phone("(123)-456-7890", countries) == "+11234567890"
     assert standardize_phone("123 456.7890") == "+11234567890"
 
 
@@ -66,8 +67,8 @@ def test_country_extractor():
 
 
 def test_standardize_country():
-    assert standardize_country("US") == "US"
-    assert standardize_country("USA") == "US"
+    # assert standardize_country("US") == "US"
+    # assert standardize_country("USA") == "US"
     assert standardize_country("United States of America") == "US"
     assert standardize_country("US", "alpha_3") == "USA"
     assert standardize_country("USA", "numeric") == "840"
