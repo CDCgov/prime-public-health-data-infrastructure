ee1b1c7148ce9b25202a3734c7a51d8e2350e77a
Nick Clyde <nick@clyde.tech>
2022-07-26 10:07:00 -0700

    Add terraform stub for Skyligh GCP

---
+++ operations/app/gcp/vars/skylight/main.tf
@@ -0,0 +1,29 @@
+
+provider "google" {
+  project = "phdi-357418"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
